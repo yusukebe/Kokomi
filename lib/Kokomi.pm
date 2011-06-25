20854863ec93355cc1835d55b5ceae317afc015f
Yusuke Wada <yusuke@kamawada.com>
2011-06-25 18:55:50 +0900

    initial commit

---
+++ lib/Kokomi.pm
@@ -0,0 +1,32 @@
+package Kokomi;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Kokomi -
+
+=head1 SYNOPSIS
+
+  use Kokomi;
+
+=head1 DESCRIPTION
+
+Kokomi is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke@kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
